8754badcfb33844ec58c5420e074f8a17302b2d0
iwonmo <310301286@qq.com>
2020-09-26 20:03:39 +0800

    修改一处细节

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,31 +11,41 @@ import org.wltea.analyzer.lucene.IKAnalyzer;
 import java.io.*;
 import java.util.*;
 
+
+
+
 public class Main {
 
+    static int index_max = 5;
 
-    static String readTextLine(File sourceFile, int lineNumber) throws IOException {
+    static List readTextLine(File sourceFile, Integer[] index) throws IOException {
+        Arrays.sort(index);
+        List<String> res = new ArrayList<String>();
         FileReader in = new FileReader(sourceFile);
         LineNumberReader reader = new LineNumberReader(in);
         String s = null;
-        int line = 1;
-            reader.setLineNumber(lineNumber);
-            long i = reader.getLineNumber();
-            while (reader.readLine() != null) {
+        Integer i = 0;
+        String red="";
+        int line = 0;
+            while ((red = reader.readLine() ) != null) {
                 line++;
-                if (i == line) {
-                    s = reader.readLine();
-                    break;
+                if(i == index.length) break;
+                int index_ = Arrays.binarySearch(index,line);
+                if(index_ > -1)
+                {
+                    i++;
+                    res.add(red);
                 }
+
             }
         reader.close();
         in.close();
-        return s;
+        return res;
     }
 
     public static void main(String[] args) throws IOException {
         HashMap<String, String> BankFind = new HashMap<String, String>();
-        FileReader fr=new FileReader("/Users/wonmo/Desktop/quickpca/支行索引.txt");
+        FileReader fr=new FileReader("/Users/wonmo/Desktop/quickpcagit/支行索引.txt");
         BufferedReader br=new BufferedReader(fr);
         String line="";
         while ((line=br.readLine())!=null) {
@@ -45,21 +55,43 @@ public class Main {
         br.close();
         fr.close();
         long startTime = System.currentTimeMillis();
-        String t = (String)BankFind.get("郑州");
-        String[] _t = t.split(",");
-        ArrayList<String> res = new ArrayList<String>();
-        File f = new File("/Users/wonmo/Desktop/quickpca/支行列表.txt");
-        int n = 0;
-        for (String item : _t){
-            n++;
-            if (n>5) break;
-            Integer i = Integer.valueOf(item);
-            String __t = readTextLine(f,i);
-            if(__t != null)
-                res.add(__t);
+        StringReader input = new StringReader("农业常熟".trim());
+        IKSegmenter ikSeg = new IKSegmenter(input, true);   // true 用智能分词 ，false细粒度
+        String t = "";
+        for (Lexeme lexeme = ikSeg.next(); lexeme != null; lexeme = ikSeg.next()) {
+            String tmp= lexeme.getLexemeText();
+            String t_ = (String)BankFind.get(tmp);
+            if(t_ != null && t_.length() > 0){
+                t = t + "," + t_;
+            }
         }
-        long endTime = System.currentTimeMillis();
-        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
-        System.out.println(res);
+
+        if(t=="")
+        {
+
+        }else{
+            String[] _t =   t.split(",");
+            File f = new File("/Users/wonmo/Desktop/quickpcagit/支行列表.txt");
+            int n = 0;
+            int index_num=index_max;
+            if(_t.length < index_max)  index_num = _t.length;
+            Integer[] intlist = new Integer[index_max]; //new Integer[_t.length];
+            int i_ = 0;
+            for(String str:_t){
+                if(i_ == index_max) break;
+                    if(!str.equals("") && Integer.parseInt(str) > 0){
+                        intlist[i_]=Integer.parseInt(str);//Exception in this line
+                        i_++;
+                }
+            }
+            List __t = readTextLine(f,intlist);
+            long endTime = System.currentTimeMillis();
+            System.out.println("程序运行时间：" + (endTime - startTime) + "ms");
+            System.out.println(__t);
+        }
+
+
+
+
     }
 }
